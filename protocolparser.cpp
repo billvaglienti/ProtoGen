5b8b1729a26df4e9924542460ebb0f9aeadae0c8
Bill Vaglienti <billvaglienti@gmail.com>
2016-08-01 09:54:51 -0700

    Fix to the mkdir() call for the documents path. Turns out that mkdir will only make a single directory, not a tree of directories. You need mkpath for that. Also mkpath returns true if the path already exists, so you don't need to test for its existence.

--- protocolparser.cpp
+++ protocolparser.cpp
@@ -17,7 +17,7 @@
 #include <iostream>
 
 // The version of the protocol generator is set here
-const QString ProtocolParser::genVersion = "1.5.3.a";
+const QString ProtocolParser::genVersion = "1.5.3.b";
 
 /*!
  * \brief ProtocolParser::ProtocolParser
