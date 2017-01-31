a579b7e8b90c713e0e3a0a4da6b96be1a774b335
Bill Vaglienti <billvaglienti@gmail.com>
2017-01-31 08:10:20 -0800

    Changed example protocol to provide an example of using protogen to create a structure used for generic packets

--- ProtoGenTest/packetinterface.h
+++ ProtoGenTest/packetinterface.h
@@ -1,16 +1,9 @@
 #ifndef PACKETINTERFACE_H
 #define PACKETINTERFACE_H
 
-/*!
- * This is a simple packet layout which is used only for testing ProtoGen. A
- * real packet layout would likely have synchronization bytes and crcs
- */
-typedef struct
-{
-    uint8_t pkttype;    // The packet type information
-    uint8_t length;     // The length of the data in bytes
-    uint8_t data[255];  // The data of the packet
+#include "DemolinkProtocol.h"
 
-}testPacket_t;
+//! Look for an incoming packet in a sequence of bytes
+int lookForDemolinkPacket(testPacket_t* pkt, uint8_t byte);
 
 #endif // PACKETINTERFACE_H
