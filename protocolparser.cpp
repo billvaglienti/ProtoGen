c1d4b94d22c22bfd64da063b3857806e8a947dce
Bill Vaglienti <billvaglienti@gmail.com>
2017-04-26 16:53:36 -0700

    Fix for minor bug in conversion of float32 to float24 and float16. If the conversion would overflow the routines were not correctly returning the maximum possible 16 or 24 bit float.

--- protocolparser.cpp
+++ protocolparser.cpp
@@ -17,7 +17,7 @@
 #include <iostream>
 
 // The version of the protocol generator is set here
-const QString ProtocolParser::genVersion = "1.9.5.b";
+const QString ProtocolParser::genVersion = "1.9.5.c";
 
 /*!
  * \brief ProtocolParser::ProtocolParser
