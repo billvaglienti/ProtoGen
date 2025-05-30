5e2a84165c5d4efcab0992ccaec8214fe4c26b93
Bill Vaglienti <billvaglienti@gmail.com>
2025-05-30 17:22:35 -0600

    Improved handling of denormal floats

--- protocolparser.cpp
+++ protocolparser.cpp
@@ -15,7 +15,7 @@
 #include <fstream>
 
 // The version of the protocol generator is set here
-const std::string ProtocolParser::genVersion = "3.6.i";
+const std::string ProtocolParser::genVersion = "3.6.j";
 
 /*!
  * \brief ProtocolParser::ProtocolParser
